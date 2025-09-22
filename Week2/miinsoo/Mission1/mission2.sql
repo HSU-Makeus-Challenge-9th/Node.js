d875e4b2b01a9703b8d9ed511856b14f69464728
miinsoo <ms1226f@gmail.com>
2025-09-22 16:06:35 +0900

    feat: Create Week2 Mission1

---
+++ Week2/miinsoo/Mission1/mission2.sql
@@ -0,0 +1,14 @@
+INSERT INTO review (
+    rating, 
+    text, 
+    created_at, 
+    store_id, 
+    user_id
+    )
+VALUES (
+    4.5, 
+    '이 가게 음식 정말 맛있어요!', 
+    NOW(), 
+    101, 
+    202
+    );
