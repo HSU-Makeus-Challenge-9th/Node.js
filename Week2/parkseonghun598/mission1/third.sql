6dc67d3243b589cd82162467eeb7c03fb9eaae3e
Parksh <2171164@hansung.ac.kr>
2025-09-29 15:48:04 +0900

    3번 수정사항 반영

--- Week2/parkseonghun598/mission1/third.sql
+++ Week2/parkseonghun598/mission1/third.sql
@@ -37,12 +37,41 @@
 
 --1,2 번 값
 --join 방식
+-- select 
+-- 	m.region,
+-- 	cm.count
+-- from missions m
+-- join complete_mission cm
+-- 	on cm.user_id = ?
+-- where m.region=?;
+
+-- --3번의 값
+-- select 
+-- 	s.name,
+-- 	s.store_type,
+-- 	datediff(mm.limited_at, now()) as remaining_day,
+-- 	--sql에서 두 날짜 사이의 차수 구함 -> 남은 일수
+-- 	s.price,
+-- 	m.point,
+-- 	concat(lpad(m.point, 10, '0'), lpad(m.id, 10, '0')) as cursor_value
+-- from missions m
+-- join stores s
+-- 	on m.store_id = s.id
+-- join mission_mapping mm
+-- 	on mm.mission_id=m.id
+-- where m.region = ?
+-- 	and mm.user_id=?
+-- 	and concat(lpad(m.point, 10, '0'), lpad(m.id, 10, '0'))< :cursor_value
+-- order by m.point desc
+-- limit 5 offset 0;
+
+-- 수정 후 쿼리 
 select 
 	m.region,
 	cm.count
 from missions m
 join complete_mission cm
-	on cm.user_id = ?
+	on cm.user_id = m.id
 where m.region=?;
 
 --3번의 값
@@ -50,10 +79,10 @@ select
 	s.name,
 	s.store_type,
 	datediff(mm.limited_at, now()) as remaining_day,
-	#sql에서 두 날짜 사이의 차수 구함 -> 남은 일수
+	--sql에서 두 날짜 사이의 차수 구함 -> 남은 일수
 	s.price,
 	m.point,
-	concat(lpad(m.point, 10, '0'), lpad(m.id, 10, '0')) as cursor_value
+	m.id as 
 from missions m
 join stores s
 	on m.store_id = s.id
@@ -61,6 +90,5 @@ join mission_mapping mm
 	on mm.mission_id=m.id
 where m.region = ?
 	and mm.user_id=?
-	and concat(lpad(m.point, 10, '0'), lpad(m.id, 10, '0'))< :cursor_value
 order by m.point desc
 limit 5 offset 0;
